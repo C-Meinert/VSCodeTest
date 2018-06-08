9ae25c4bbd6153fe7b24dd7ec95e25227ba33bcb
C-Meinert <cmeinert89@gmail.com>
2018-06-08 17:52:00 -0400

    Added .NetCore console app

---
+++ testNetCoreApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testNetCoreApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
